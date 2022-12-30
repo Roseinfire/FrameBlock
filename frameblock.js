5875a582c8564c87796eb420941edc191ba9e96a
Roseinfire <14box14@gmail.com>
2022-12-30 14:50:51 +0300

    Update frameblock.js

--- frameblock.js
+++ frameblock.js
@@ -1,10 +1,10 @@
 setInterval(function block() {
-   window.open = function() {  }
    var fs = []
    var iframes = document.getElementsByTagName("iframe")
    var frames = document.getElementsByTagName("frame")
    var links = document.getElementsByTagName("a")
    var yatags = document.getElementsByTagName("yatag")
+   var roots = document.body.children
    for(var i = 0; i < iframes.length; i++) { 
      fs.push(iframes[i]); iframes[i].parentElement.removeChild(iframes[i]);
      }
@@ -12,12 +12,19 @@ setInterval(function block() {
      fs.push(frames[i]); frames[i].parentElement.removeChild(frames[i]);
      };
    for(var i = 0; i < links.length; i++) {
-     if(links[i].getElementsByTagName("img").length) {
+     if(links[i].getElementsByTagName("img").length || links[i].getElementsByTagName("video").length) {
      fs.push(links[i]); links[i].parentElement.removeChild(links[i]);
         }
      };
     for(var i = 0; i < yatags.length; i++) {
        fs.push(yatags[i]); yatags[i].parentElement.removeChild(yatags[i]);
      };
+    for(var i = 0; i < roots.length; i++) { 
+      var dv = roots[i]
+             if(dv.shadowRoot && !dv.mark) {
+               dv.style.display = "none"
+               fs.push(dv); dv.mark = true
+             } 
+       }
      if(fs.length) { console.warn("frames blocked >> ", fs) }
   }, 50)
