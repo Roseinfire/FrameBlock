023da7a779d125a1922a0e30105d2b56b48d0f8e
Roseinfire <14box14@gmail.com>
2023-01-28 23:33:59 +0300

    Update frameblock.js

--- frameblock.js
+++ frameblock.js
@@ -1,17 +1,36 @@
+function insert(where, element, before) {
+ if(before) {
+   where.insertBefore(element, before)
+     }
+   else {
+     where.append(element)
+      }
+  }
 var methods = new Array()
-setInterval(function block() {
-      methods.push({ tag: 'frame', filter: function(e) { return e } })
-      methods.push({ tag: 'iframe', filter: function(e) { return e } })  
-      methods.push({ tag: 'yatag', filter: function(e) { return e } })
+      methods.push({ tag: 'frame', filter: function(e) { return true } })
+      methods.push({ tag: 'iframe', filter: function(e) { return true } })  
+      methods.push({ tag: 'yatag', filter: function(e) { return true } })
       methods.push({ tag: 'a', filter: function(e) { 
                   if(e.getElementsByTagName("img").length || e.getElementsByTagName("video").length) {
                         return e
                         }
                   } })
       methods.push({ tag: 'div', filter: function(e) {
-                  e.parentElement.append(e.cloneNode(49))
-                  return e
-                  } })
+                if(!e.children.length) {
+                    var hand = e.parentElement
+                    var group = hand.children
+                    var place = 0
+                    while(place < group.length) {
+                        if(group[place] == e) { break }
+                        else { place++ }
+                    }
+                  hand.removeChild(e)
+                  insert(hand, e.cloneNode(29), group[place+1])
+                  return false
+                     }
+                   } 
+                })
+setTimeout(function block() {
       var blocked = new Array()
       function remove(tag, filter=function() { return true }) {
             var things; (typeof tag == 'string') ? things = document.getElementsByTagName(tag) : things = tag
