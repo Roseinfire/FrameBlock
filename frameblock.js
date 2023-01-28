65463f20b6e5625f4697ab40a6656d0a1197523c
Roseinfire <14box14@gmail.com>
2023-01-28 18:17:39 +0300

    Update frameblock.js

--- frameblock.js
+++ frameblock.js
@@ -1,29 +1,25 @@
 var methods = new Array()
-var times = 0
 setInterval(function block() {
-  methods.push({ tag: 'frame', filter: function(e) { return true } })
-  methods.push({ tag: 'iframe', filter: function(e) { return true } })  
-  methods.push({ tag: 'yatag', filter: function(e) { return true } })
-  methods.push({ tag: 'a', filter: function(e) { 
-    if(e.getElementsByTagName("img").length || e.getElementsByTagName("video").length) {
-     return true
-        }
-  } })
-    times++; (function removeShadows() {
-     if(times % 7 == 0) { 
-      var body = document.body.cloneNode(99)
-      var html = document.body.parentElement
-      html.removeChild(document.body); html.appendChild(body)
-        }
-      })()
-   var blocked = new Array()
-   function remove(tag, filter=function() { return true }) {
-      var things; (typeof tag == 'string') ? things = document.getElementsByTagName(tag) : things = tag
-      for(var i = 0; i < things.length; i++) {
-         if(filter(things[i])) { blocked.push(things[i]); things[i].parentElement.removeChild(things[i]); }
-          }
-      }
-   for(var i = 0; i < methods.length; i++) {
-      remove(methods[i].tag, methods[i].filter)
-    }; if(blocked.length) { console.log("frames blocked > ", blocked) }
- }, 200)
+      methods.push({ tag: 'frame', filter: function(e) { return e } })
+      methods.push({ tag: 'iframe', filter: function(e) { return e } })  
+      methods.push({ tag: 'yatag', filter: function(e) { return e } })
+      methods.push({ tag: 'a', filter: function(e) { 
+                  if(e.getElementsByTagName("img").length || e.getElementsByTagName("video").length) {
+                        return e
+                        }
+                  } })
+      methods.push({ tag: 'div', filter: function(e) {
+                  e.parentElement.append(e.cloneNode(49))
+                  return e
+                  } })
+      var blocked = new Array()
+      function remove(tag, filter=function() { return true }) {
+            var things; (typeof tag == 'string') ? things = document.getElementsByTagName(tag) : things = tag
+            for(var i = 0; i < things.length; i++) {
+                  if(filter(things[i])) { blocked.push(things[i]); things[i].parentElement.removeChild(things[i]); }
+                  }
+            }
+      for(var i = 0; i < methods.length; i++) {
+            remove(methods[i].tag, methods[i].filter)
+            }; if(blocked.length) { console.log("frames blocked > ", blocked) }
+      }, 500)
